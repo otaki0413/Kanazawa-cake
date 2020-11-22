f4624125c1a7824c075bfbb332804206690b2d4d
EC2 Default User <ec2-user@ip-172-31-40-230.ec2.internal>
2020-11-22 15:18:58 +0000

    Finish admin-top

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :members
+
+  get '/admin' => 'admin/homes#top'
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+  }
+  # devise_for :admin, skip: :all
+  # devise_scope :admin do
+  #   get 'admin/sign_in' => 'admin/sessions#new'
+  #   post 'admin/sign_in' => 'admin/sessions#create'
+  #   delete 'admin/sign_out' => 'admin/sessions#destroy'
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
