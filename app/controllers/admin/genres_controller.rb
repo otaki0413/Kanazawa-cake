c54e8929f133fbfa6b26245cecb620dc306a8af1
EC2 Default User <ec2-user@ip-172-31-40-230.ec2.internal>
2020-11-25 09:04:38 +0000

    Update admin-product

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,44 @@
+class Admin::GenresController < ApplicationController
+
+  # ログイン認証されていなければ、ログイン画面へリダイレクト
+  before_action :authenticate_admin!
+
+  def index
+    @newgenre = Genre.new
+    @genres = Genre.all
+  end
+
+  # ジャンル新規登録
+  def create
+    @newgenre = Genre.new(genre_params)
+    @newgenre.save
+    flash[:notice] = "You have created genre successfully."
+    redirect_to admin_genres_path
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    flash[:notice] = "You have updated genre successfully."
+    redirect_to admin_genres_path
+  end
+
+  # ジャンル削除
+  def destroy
+    genre = Genre.find(params[:id])
+    genre.destroy
+    redirect_to admin_genres_path
+  end
+
+  # ストロングパラメータ
+  private
+  def genre_params
+    params.require(:genre).permit(:name, :is_active)
+  end
+
+end
+
